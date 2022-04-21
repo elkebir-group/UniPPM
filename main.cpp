2f5cff9de8626f43840b4117df73e640dce27193
Yuanyuan Qi <yuanyuanqi@wirelessprv-10-192-54-75.near.illinois.edu>
2022-04-21 12:34:20 -0500

    rejection

--- main.cpp
+++ main.cpp
@@ -200,7 +200,7 @@ int main(int argc, char * argv[]) {
     }
     cout<<"[UniPPM] get "<< tmp << " sols from 10000 rej attempt."<<endl;
     res.clear();
-    if(tmp>=5){
+    if(tmp>=1){
         rej.sample(n_samples*2, res);
     }
     else {
